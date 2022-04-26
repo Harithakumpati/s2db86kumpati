2fe6b2832c8af1649cf4170d7f386936841b4ed4
Harithakumpati <98130767+Harithakumpati@users.noreply.github.com>
2022-04-25 22:27:12 -0500

    updated

--- models/house.js
+++ models/house.js
@@ -1,7 +1,16 @@
 const mongoose = require("mongoose")
 const houseSchema = mongoose.Schema({
-    house_name: String,
-    house_color: String,
-    house_cost: Number
+    house_name:{
+        type: String,
+        minLenght: 7
+    },
+    house_color:{
+        type: String,
+        minLenght: 5
+    },
+    house_cost:{
+        type: Number,
+        minLenght: 6
+    }, 
 })
 module.exports = mongoose.model("House", houseSchema)
